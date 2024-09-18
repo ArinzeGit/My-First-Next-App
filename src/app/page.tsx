99fd834cd17b00d0f04ecd61dc592c64cbe92699
Arinze Owoh <arinzeowoh@gmail.com>
2024-09-18 01:05:54 +0100

    add layout, api routes, Image and Link elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "@/components/Header";
 import Button from "@/components/Button";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,6 +10,15 @@ export default function Home() {
       <p className="mt-4">Welcome to the homepage!</p>
       <h1 className="text-2xl font-bold">Welcome to My Next.js App</h1>
       <Button label="Click Me" />
+      <Image
+        src="/images/Arinze in suit.jpg"
+        alt="Arinze in a suit"
+        width={800}
+        height={800}
+      />
+      <Link href="/about" className="text-blue-500 underline">
+        About Us
+      </Link>
     </div>
   );
 }
